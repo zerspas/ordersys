2d876a86cc2e7718531e947abe55990bf6cf41c9
Administrator <Administrator@ZJZL-20190808DV>
2019-10-25 10:37:01 +0800

    2019-10-25 组长张晟初始化项目

---
+++ OrdersysManager/src/main/java/com/ordersys/OrdersysManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ordersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersysManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersysManagerApplication.class, args);
+	}
+
+}
